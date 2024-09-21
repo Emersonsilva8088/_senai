deb39436791388942158a9fd9dc34595bf799cd0
Emersonsilva <159089774+Emersonsilva8088@users.noreply.github.com>
2024-09-21 10:29:04 -0300

    comit

---
+++ classes/pessoa.cs
@@ -0,0 +1,8 @@
+namespace _senai.classes
+{
+    public abstract class pessoa
+    {
+        public string? Nome {get; set;}
+        public float Rendimento {get; set;}
+    }
+}
---
+++ classes/pessoajuridica.cs
@@ -0,0 +1,12 @@
+
+namespace _senai.classes
+{
+    public class pessoajuridica : pessoa
+    {
+        public string? cnpj {get; set;}
+        public void Inserir (pessoajuridica pj) {
+
+        }
+
+    }
+}
